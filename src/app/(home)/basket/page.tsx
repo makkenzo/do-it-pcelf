d4d2666f39bfe25d7b4165096154a38d24e7231d
vladgerman27 <vladg160.mail@gmail.com>
2024-03-29 10:12:02 +0500

    eighth

--- src/app/(home)/basket/page.tsx
+++ src/app/(home)/basket/page.tsx
@@ -9,23 +9,35 @@ import { ICard } from '@/types';
 import Image from 'next/image';
 
 import Trash from '../../../../public/img/trash.png'
+import { useUser } from "@clerk/nextjs";
 
 interface BasketProps {}
 
 const Basket = ({}: BasketProps) => {
-  const [data, setData] = useState<ICard[]>([]);
+  const [items, setItems] = useState<ICard[]>([]);
+
+  const {user} = useUser()
 
   useEffect(() => {
     const fetchData = async () => {
-        const response = await server.get('/products/get-products', { params: { page: 1, limit: 8 } });
-
-        if (response.status === 200) {
-            setData(response.data.data);
+        if (user) {
+            try {
+                const response = await server.get(`/user/get-cart/${user.id}`);
+                setItems(response.data.items);
+            } catch (error) {
+                console.error(error);
+            }
         }
     };
 
     fetchData();
-}, []);
+}, [user]);
+
+let totalPrice = items.reduce((total, item) => total + item.price, 0);
+
+  console.log(totalPrice);
+    
+console.log(items);
 
   return (
     <div>
@@ -34,10 +46,8 @@ const Basket = ({}: BasketProps) => {
        <div className='w-full border-t-2 border-blue-500'>
           <div className='flex justify-between px-28 pt-12'>
             <div className="block w-6/12">
-              {data &&
-                data.length > 0 &&
-                data.map((item, index) => (
-                    <div key={index} className="w-full bg-white flex p-7 rounded-lg mb-6">
+              {items && items.map(item => (
+                    <div key={item._id} className="w-full bg-white flex p-7 rounded-lg mb-6">
                         <Image src={item.image} alt="card" width={182} height={196}/>
                         <div className="ml-6">
                           <nav className="max-w-md">{item.name}</nav>
@@ -47,8 +57,6 @@ const Basket = ({}: BasketProps) => {
                               <button className="mr-6">
                                 <img src={Trash.src} className="w-6 h-6"/>
                               </button>
-                              <button className="mr-6">-</button>
-                              <button className="ml-6 duration-300">+</button>
                             </div>
                           </div>
                         </div>
@@ -58,10 +66,8 @@ const Basket = ({}: BasketProps) => {
 
             <div className="w-1/3 bg-white px-8 py-4 rounded-lg">
                   <nav className="text-3xl">Ваш заказ</nav>
-                  {data &&
-                    data.length > 0 &&
-                    data.map((item, index) => (
-                        <div key={index} className="mt-8 w-full flex justify-between text-neutral-500 text-base">
+                  {items && items.map(item => (
+                        <div key={item._id} className="mt-8 w-full flex justify-between text-neutral-500 text-base">
                             <nav>{item.name.slice(0, 30) + '...'}</nav>
                             <nav>{item.price.toLocaleString('ru-RU')}тг</nav>
                         </div>
@@ -69,7 +75,7 @@ const Basket = ({}: BasketProps) => {
 
                   <div className="pt-3 w-full flex justify-between border-neutral-500 border-t-2">
                     <nav>Итого</nav>
-                    <nav>тг</nav>
+                    <nav>{totalPrice.toLocaleString('ru-RU')}тг</nav>
                   </div>     
             </div>
           </div>
